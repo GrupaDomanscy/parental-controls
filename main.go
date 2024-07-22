b58d441c3e5d86ba90391c25e702764fe319bb6a
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-07-22 19:43:53 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"domanscy.group/gui"
+	. "domanscy.group/gui/components"
+)
+
+func main() {
+	text := NewTextComponent("Hello world! How are you today?", "Roboto", 32, 0, gui.WhiteColor)
+
+	gui.BuildApp().
+		WithTitle("Hello world").
+		WithInitialSize(800, 600).
+		WithFont("Roboto", "assets/Roboto-Regular.ttf").
+		WithRootElement(text).
+		Run()
+}
