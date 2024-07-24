25ae1e23e355cfaa43fc43d698f87a7b4af69b52
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-07-25 00:41:38 +0200

    Add layout

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func main() {
+	layout := NewLayoutComponent(DirectionRow, AlignStart, AlignStart)
+
 	text := NewTextComponent("Hello world! How are you today?", "Roboto", 32, 0, gui.WhiteColor)
+	text2 := NewTextComponent("Hello world! How are you today?", "Roboto", 32, 0, gui.WhiteColor)
+	text3 := NewTextComponent("Hello world! How are you today?", "Roboto", 32, 0, gui.WhiteColor)
+
+	layout.AddChild(text)
+	layout.AddChild(text2)
+	layout.AddChild(text3)
 
 	gui.BuildApp().
 		WithTitle("Hello world").
 		WithInitialSize(800, 600).
 		WithFont("Roboto", "assets/Roboto-Regular.ttf").
-		WithRootElement(text).
+		WithRootElement(layout).
 		Run()
 }
