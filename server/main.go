3782a027836ac79f9a98509168946401a8748179
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-08-23 15:02:49 +0200

    change method of getting env vars

--- server/main.go
+++ server/main.go
@@ -3,30 +3,31 @@ package main
 import (
 	"crypto/rsa"
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"domanscy.group/env"
 	"domanscy.group/parental-controls/server/database"
 	"domanscy.group/parental-controls/server/users"
 	"domanscy.group/rckstrvcache"
-	"fmt"
 	"github.com/go-chi/chi"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
-	"net/http"
-	"time"
 )
 
 type ServerConfig struct {
-	AppUrl        string `env:"APP_URL"`
-	ServerAddress string `env:"SERVER_ADDRESS"`
-	ServerPort    uint16 `env:"SERVER_PORT"`
+	AppUrl        string
+	ServerAddress string
+	ServerPort    uint16
 
-	EmailFromAddress string `env:"EMAIL_FROM_ADDRESS"`
-	SmtpAddress      string `env:"SMTP_ADDRESS"`
-	SmtpPort         uint16 `env:"SMTP_PORT"`
+	EmailFromAddress string
+	SmtpAddress      string
+	SmtpPort         uint16
 
-	BearerTokenPrivateKey rsa.PrivateKey `env:"BEARER_TOKEN_PRIVATE_KEY" env_type:"PRIVATE_KEY"`
+	BearerTokenPrivateKey *rsa.PrivateKey
 
-	DatabaseUrl string `env:"DATABASE_URL"`
+	DatabaseUrl string
 }
 
 func NewServer(cfg ServerConfig, regkeysStore *rckstrvcache.Store, oneTimeAccessTokenStore *rckstrvcache.Store, db *sql.DB) http.Handler {
@@ -48,16 +49,90 @@ func startServer(cfg ServerConfig, regkeysStore *rckstrvcache.Store, otatStore *
 	}
 }
 
-func main() {
-	log.SetFlags(log.Ldate | log.LUTC | log.Lmicroseconds | log.Llongfile)
+func readConfig() ServerConfig {
+	appUrl, exists, err := env.ParseValidUrlWithHttpOrHttpsProtocol("APP_URL")
+	if !exists {
+		log.Fatalf("env '%s' is required", "APP_URL")
+	}
 
-	cfg := ServerConfig{}
-	env.ReadToCfg(&cfg)
+	if err != nil {
+		log.Fatalf("env '%s' parsing error: %v", err)
+	}
+
+	appUrlWithoutTrailingSlash := appUrl.String()
 
 	// remove trailing slash
-	if cfg.AppUrl[len(cfg.AppUrl)-1] == '/' {
-		cfg.AppUrl = cfg.AppUrl[:len(cfg.AppUrl)-1]
+	if appUrlWithoutTrailingSlash[len(appUrlWithoutTrailingSlash)-1] == '/' {
+		appUrlWithoutTrailingSlash = appUrlWithoutTrailingSlash[:len(appUrlWithoutTrailingSlash)-1]
 	}
+
+	serverAddress, exists := env.ParseStringVar("SERVER_ADDRESS")
+	if !exists {
+		log.Fatalf("env '%s' is required", "APP_URL")
+	}
+
+	serverPort, exists, err := env.ParseUint16Var("SERVER_PORT")
+	if !exists {
+		log.Fatalf("env '%s' is required", "SERVER_PORT")
+	}
+
+	if err != nil {
+		log.Fatalf("env '%s' parsing error: %v", err)
+	}
+
+	emailFromAddress, exists := env.ParseStringVar("EMAIL_FROM_ADDRESS")
+	if !exists {
+		log.Fatalf("env '%s' is required", "EMAIL_FROM_ADDRESS")
+	}
+
+	smtpAddress, exists, err := env.ParseUrlVarOnlyWithHost("SMTP_ADDRESS")
+	if !exists {
+		log.Fatalf("env '%s' is required", "EMAIL_FROM_ADDRESS")
+	}
+
+	if err != nil {
+		log.Fatalf("env '%s' parsing error: %v", err)
+	}
+
+	smtpPort, exists, err := env.ParseUint16Var("SMTP_PORT")
+	if !exists {
+		log.Fatalf("env '%s' is required", "SMTP_PORT")
+	}
+
+	if err != nil {
+		log.Fatalf("env '%s' parsing error: %v", err)
+	}
+
+	bearerTokenPrivateKey, exists, err := env.ParsePrivateKeyVarFromFilePath("BEARER_TOKEN_PRIVATE_KEY")
+	if !exists {
+		log.Fatalf("env '%s' is required", "BEARER_TOKEN_PRIVATE_KEY")
+	}
+
+	if err != nil {
+		log.Fatalf("env '%s' parsing error: %v", err)
+	}
+
+	databaseUrl, exists := env.ParseStringVar("DATABASE_URL")
+	if !exists {
+		log.Fatalf("env '%s' is required", "BEARER_TOKEN_PRIVATE_KEY")
+	}
+
+	cfg := ServerConfig{
+		AppUrl:                appUrlWithoutTrailingSlash,
+		ServerAddress:         serverAddress,
+		ServerPort:            serverPort,
+		EmailFromAddress:      emailFromAddress,
+		SmtpAddress:           smtpAddress.String(),
+		SmtpPort:              smtpPort,
+		BearerTokenPrivateKey: bearerTokenPrivateKey,
+		DatabaseUrl:           databaseUrl,
+	}
+
+	return cfg
+}
+
+func main() {
+	log.SetFlags(log.Ldate | log.LUTC | log.Lmicroseconds | log.Llongfile)
 
 	regkeysStore, regkeyErrCh, err := rckstrvcache.InitializeStore(time.Minute * 15)
 	if err != nil {
